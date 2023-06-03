2422f82a090a49d51cf7a65614a5aa4a4aba5ab4
desuex <nnm043@gmail.com>
2023-06-04 00:52:36 +0600

    boost is working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    boost::filesystem::path path("opensr2");
+    if (boost::filesystem::exists(path)) {
+        std::cout << "File exists!" << std::endl;
+    } else {
+        std::cout << "File does not exist!" << std::endl;
+    }
+
     return 0;
 }
