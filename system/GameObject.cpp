6d5de1aef8c9687c29d9497f2e6e58fb4281e1e4
desuex <nnm043@gmail.com>
2023-06-07 02:38:06 +0600

    add gameobject as system lib

---
+++ system/GameObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ellis May on 07.06.2023.
+//
+
+#include "GameObject.h"
---
+++ system/GameObject.h
@@ -0,0 +1,14 @@
+//
+// Created by Ellis May on 07.06.2023.
+//
+
+#ifndef OPENSR2_GAMEOBJECT_H
+#define OPENSR2_GAMEOBJECT_H
+
+
+class GameObject {
+
+};
+
+
+#endif //OPENSR2_GAMEOBJECT_H
